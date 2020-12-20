d228983c12daec32e28f67c6c28c0280ddcebe77
brunobannwart <bbannwart@yahoo.com.br>
2020-12-20 13:22:20 -0300

    Acrescimo de colunas nas tabelas

--- backend/src/libs/Database.py
+++ backend/src/libs/Database.py
@@ -2,13 +2,13 @@ import mysql.connector as mysql
 
 class Database:
 	def __init__(self):
-		self.connection = mysql.connect(host='localhost', user='ceasa', password='projeto$ceasa', database='db_ceasa', auth_plugin='mysql_native_password')
-		self.cursor = self.connection.cursor()
+		self.conexao = mysql.connect(host='localhost', user='ceasa', password='projeto$ceasa', database='db_ceasa', auth_plugin='mysql_native_password')
+		self.cursor = self.conexao.cursor()
 		self.__formarTabelas()
 
 	def __formarTabelas(self):
-		self.cursor.execute("CREATE TABLE IF NOT EXISTS `romaneio`(`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, `codigo_escola` INTEGER, `numero_romaneio` VARCHAR(100), `data_hora` DATETIME, `codigo_produto` VARCHAR(12), `quantidade` INTEGER, `codigo_usuario` INTEGER) ENGINE=InnoDB")
-		self.cursor.execute("CREATE TABLE IF NOT EXISTS `requisicao`(`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, `codigo_escola` INTEGER, `numero_documento` VARCHAR(100), `data_hora` DATETIME, `tipo` VARCHAR(2), `E/S` VARCHAR(1), `codigo_produto` VARCHAR(12), `quantidade` INTEGER, `codigo_usuario` INTEGER) ENGINE=InnoDB")
+		self.cursor.execute("CREATE TABLE IF NOT EXISTS `romaneio`(`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, `codigo_escola` INT, `numero_romaneio` VARCHAR(100), `sequencia` INT, `data_hora` DATETIME, `codigo_produto` VARCHAR(12), `quantidade` INT, `codigo_usuario` INT) ENGINE=InnoDB")
+		self.cursor.execute("CREATE TABLE IF NOT EXISTS `requisicao`(`id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, `codigo_escola` INT, `numero_documento` INT, `data_hora` DATETIME, `tipo` VARCHAR(2), `entrada_saida` VARCHAR(1), `codigo_produto` VARCHAR(12), `quantidade` INT, `codigo_usuario` INT) ENGINE=InnoDB")
 
 	def obterUsuario(self, cpf):
 		try:
@@ -37,6 +37,7 @@ class Database:
 	def alterarSenhaUsuario(self, cpf, senha):
 		try:
 			self.cursor.execute("UPDATE `usuario` SET `senha_hash`=%s WHERE `cpf`=%s", [senha, cpf])
+			self.conexao.commit()
 			return True
 
 		except:
@@ -105,3 +106,74 @@ class Database:
 
 		except:
 			return []
+
+	def obterProduto(self, codigoProduto):
+		try:
+			self.cursor.execute("SELECT * FROM `produto` WHERE `codigo`=%s", [codigoProduto])
+			resultado = self.cursor.fetchone()
+
+			if resultado != None:
+				produto = {
+					'id': resultado[0],
+					'codigo': resultado[1],
+					'descricao': resultado[2],
+				}
+
+				return produto
+
+			else:
+				return None
+
+		except:
+			return None
+
+	def obterEstoque(self, escolaID, produtoID):
+		try:
+			self.cursor.execute("SELECT * FROM `estoque` WHERE `escola_id`=%s AND `produto_id`=%s", [escolaID, produtoID])
+			resultado = self.cursor.fetchone()
+
+			if resultado != None:
+				estoque = {
+					'id': resultado[0],
+					'escola': resultado[1],
+					'produto': resultado[2],
+					'quantidade': resultado[3],
+					'minimo': resultado[4],
+				}
+
+				return estoque
+			
+			else:
+				return None
+
+		except:
+			return None
+
+	def atualizarEstoque(self, estoqueID, quantidade):
+		try:
+			self.cursor.execute("UPDATE `estoque` SET `quantidade`=%s WHERE `id`=%s", [quantidade, estoqueID])
+			self.conexao.commit()
+			return True
+
+		except:
+			return False
+
+	def cadastrarRomaneio(self, codigoEscola, numeroRomaneio, sequencia, dataHora, codigoProduto, quantidade, codigoUsuario):
+		try:
+			self.cursor.execute("INSERT INTO `romaneio` (`codigo_escola`, `numero_romaneio`, `sequencia`, `data_hora, `codigo_produto`, `quantidade`, `codigo_usuario`) VALUES (%s, %s, %s, %s, %s, %s, %s)", 
+				[codigoEscola, numeroRomaneio, sequencia, dataHora, codigoProduto, quantidade, codigoUsuario])
+			self.conexao.commit()
+			return True
+
+		except:
+			return False
+
+	def cadastrarRequisição(self, codigoEscola, numeroDocumento, dataHora, tipo, eS, codigoProduto, quantidade, codigoUsuario):
+		try:
+			self.cursor.execute("INSERT INTO `requisicao` (`codigo_escola`, `numero_documento`, `data_hora, `tipo`, `entrada_saida`, `codigo_produto`, `quantidade`, `codigo_usuario`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", 
+				[codigoEscola, numeroDocumento, dataHora, tipo, eS, codigoProduto, quantidade, codigoUsuario])
+			self.conexao.commit()
+			return True
+
+		except:
+			return False
